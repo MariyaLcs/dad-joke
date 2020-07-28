2ea651bd70ccb89b55473e4bb2b598080180eb48
Mariya <mariya.podosinova@gmail.com>
2020-07-28 18:19:41 +0100

    Updates

---
+++ precache-manifest.a535ee3ecce7fe572d056210750d8dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd6b04fc02bc791db84df072ed5a95f",
+    "url": "/dad-joke/index.html"
+  },
+  {
+    "revision": "c65bc5ff32c73a98243c",
+    "url": "/dad-joke/static/css/main.98c63301.chunk.css"
+  },
+  {
+    "revision": "550229547f6f01a39a0a",
+    "url": "/dad-joke/static/js/2.b1228249.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-joke/static/js/2.b1228249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65bc5ff32c73a98243c",
+    "url": "/dad-joke/static/js/main.00117c30.chunk.js"
+  },
+  {
+    "revision": "f0919a297cc2583bdf0d",
+    "url": "/dad-joke/static/js/runtime-main.31cd1aaa.js"
+  }
+]);
